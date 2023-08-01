c9364db513ef3dc48f0916daa7e2d9829a637bfa
Chandler Abril <abrilchandler1@gmail.com>
2023-08-01 13:47:41 -0700

    hw#20

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const main = document.querySelector("main")
+
+document.body.removeChild(main)
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory"
+
+newHeader.textContent = "Chandler is the champion"
+
+document.body.append(newHeader);
+
